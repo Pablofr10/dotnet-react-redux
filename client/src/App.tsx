1457fa8ac4790dfd9282d0769a1fe2f108994899
Pablorf10 <rafanet.live@gmail.com>
2021-10-29 22:55:00 -0300

    Instaling react in project

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+const products = [
+  {name: }
+]
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Re-Store </h1>
+      {console.log("Hello the app")}
+    </div>
+  );
+}
+
+export default App;
