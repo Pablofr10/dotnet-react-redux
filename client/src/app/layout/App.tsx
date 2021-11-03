87efb67a2c59d8cfcdd35ebafaa1eee77bf4c4c3
Pablorf10 <rafanet.live@gmail.com>
2021-11-02 22:18:03 -0300

    Adding dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,40 +1,31 @@
-import { Container, CssBaseline, Typography } from "@material-ui/core";
-import { useEffect, useState } from "react";
+import { ThemeProvider } from "@emotion/react";
+import { Container, createTheme, CssBaseline } from "@material-ui/core";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? "dark" : "light";
 
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+    },
+  });
 
-  function addProduct() {
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        id: prevState.length + 101,
-        name: "product" + (prevState.length + 1),
-        price: prevState.length * 100 + 100,
-        brand: "some brand",
-        description: "some description",
-        pictureUrl: "http://picsum.photos/200",
-      },
-    ]);
+  function changeDarkMode() {
+    setDarkMode(!darkMode);
   }
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header changeDarkMode={changeDarkMode} />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,10 +1,15 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Switch, Toolbar, Typography } from "@material-ui/core";
 
-export default function Header() {
+interface Props {
+  changeDarkMode: () => void;
+}
+
+export default function Header({ changeDarkMode }: Props) {
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
       <Toolbar>
         <Typography variant="h6">RE-STORE</Typography>
+        <Switch onChange={changeDarkMode} color="default" />
       </Toolbar>
     </AppBar>
   );
