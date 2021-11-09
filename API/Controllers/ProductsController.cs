8f2eeedbdcd398210fd8e692920ed89916a7e1c0
Pablorf10 <rafanet.live@gmail.com>
2021-11-08 22:59:12 -0300

    Criando controller para capturar errros

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,9 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseController
     {
         private readonly StoreContext _context;
         public ProductsController(StoreContext context)
